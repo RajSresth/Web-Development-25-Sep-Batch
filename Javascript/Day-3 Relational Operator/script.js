4f79325d79fa7cd6fc755debc6f174ac32247602
Shresth Rajput <rajshresth2@gmail.com>
2024-12-12 13:26:32 +0530

    Conditional Operator

---
+++ Javascript/Day-3 Relational Operator/script.js
@@ -0,0 +1,25 @@
+// console.log("a">=10)
+// console.log("a"<=10)
+
+// const n1="0x10"
+// const n2=16;
+// console.log(n1==n2)
+
+// const x=+"abc";
+// const y=+"abc";
+// console.log(x === y)
+
+/**
+ * ! same type undefined
+ */
+
+// var x=null;
+// var y=null;
+// console.log(x === y)
+
+let x="";
+let y=[];
+
+
+console.log(x == y)  //true
+console.log(x === y)  //false
---
+++ Javascript/Day-4 Conditional Operator/script.js
@@ -0,0 +1,32 @@
+// const user=prompt("Enter your name");
+// console.log(user)
+
+// const res=(user && user!==" ")?`Welcome ${user}`:`Invalid User Name`;
+// console.log(res)
+
+
+/**
+ * TODO: Question 1 write a js program to find biggest of three numbers?
+
+
+const a=+prompt("Enter first number");  //5
+const b=+prompt("Enter second number");  //10
+const c=+prompt("Enter last number"); //6
+
+const result=(a>b?a:b)>c?(a>b?a:b):c;
+
+console.log(`Biggest number among ${a}, ${b} and ${c} is: ${result}`);
+ */
+
+/**
+ * TODO: Question2 write a js program to find leap year?
+* ! leap year= year%4;
+* ! centuary year= year % 400
+ */
+
+const year=+prompt("Enter a year");
+
+const res= (year%100 !==0 && year%4 ===0) || (year%400===0)?`Leap Year ${year}`:` ${year} Not a leap Year`;
+
+console.log(res);
+
