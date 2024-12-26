4756ed34e91a8ff531f6da0202e2a5a5e8df2e10
Shresth Rajput <rajshresth2@gmail.com>
2024-12-26 13:11:50 +0530

    Function Declaration Updated

--- Javascript/Day-8 Function in JS/1. Function Declaration Statement/script2.js
+++ Javascript/Day-8 Function in JS/1. Function Declaration Statement/script2.js
@@ -1,3 +1,7 @@
+/**
+ * ! Case1: Function is declared inside a block
+ 
+
 var user="Tinku";
 
     {
@@ -14,3 +18,73 @@ var user="Tinku";
 
 let x=details(21,"Noida")
 console.log(x)
+*/
+
+/* 
+ !Function is Declared inside another function
+
+
+var user="Tinku";
+
+    function outer(age,city)
+    {
+        console.log("Inside outer Function");
+        var pin=110006;
+        console.log(`Username: ${user}`)
+
+        function inner(m,n)
+        {
+                console.log("Inside inner Function")
+                console.log(m)
+                console.log(n)
+        }
+
+        inner()
+    }
+
+outer()
+*/
+
+
+/* 
+! Variable shadowning 
+var fname="Chombu";
+function fun1()
+{
+    var fname="Dinga";
+  console.log(`Username: ${fname}`)
+  console.log(`Username: ${window.fname}`)
+}
+fun1()
+*/
+
+/* 
+!  Default Parameters
+
+function details(user)
+{
+    if(user.trim() !=="" && user.length!==0)
+    {
+
+        console.log(`Username: ${user.trim()}`)
+    }
+    else{
+        console.error("Invalid username")
+    }
+}
+
+let username=prompt("Enter fullname");
+details(username)
+
+*/
+
+/*
+ ! Arguments
+*/
+function memes()
+{
+    
+    console.log(`${arguments[0]+" or "+arguments[1]} Bulati h magar jane ka nahi..!`)
+}
+
+memes("Paras","Abhishek")
