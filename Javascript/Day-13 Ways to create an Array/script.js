cacc0b3421e30f781faded8cb88f492d47b20000
Shresth Rajput <rajshresth2@gmail.com>
2025-01-10 13:39:07 +0530

    Array Basics

---
+++ Javascript/Day-13 Ways to create an Array/script.js
@@ -0,0 +1,20 @@
+
+/*
+! push()
+const skills=["html","css","js"];
+let x=skills.push("react","node")
+
+console.log(skills)
+console.log(x)
+*/
+
+/*
+! pop method */
+
+const arr2=[10,20,30,400]
+
+let y=arr2.pop();
+console.log(arr2)
+console.log("Returned value",y)
+
+forEach(),map(),reduce(),filter()
