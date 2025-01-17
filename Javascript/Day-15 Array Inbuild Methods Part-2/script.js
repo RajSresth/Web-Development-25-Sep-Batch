ca95bb1d2b9e50262977c37abc5137a23e922fdc
Shresth Rajput <rajshresth2@gmail.com>
2025-01-17 12:39:47 +0530

    Foreach Method

---
+++ Javascript/Day-15 Array Inbuild Methods Part-2/script.js
@@ -0,0 +1,19 @@
+// ! sort(callback)
+
+
+let arr=[4,2,5,1,3]
+
+
+arr.sort((a,b)=>b-a)
+
+// +ve swap
+// -ve no swap
+// 0 no swap
+
+console.log(arr)
+
+
+const skills=["html","css","js"];
+skills.sort()
+console.log(skills)
+
