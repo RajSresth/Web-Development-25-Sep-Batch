3e4d11db2e9f2535645f98f08b818dbf0c09aca5
Shresth Rajput <rajshresth2@gmail.com>
2025-01-20 14:57:56 +0530

    Lecture on filter and reduce Method

--- Javascript/Day-15 Array Inbuild Methods Part-2/script.js
+++ Javascript/Day-15 Array Inbuild Methods Part-2/script.js
@@ -1,19 +1,52 @@
 // ! sort(callback)
 
 
-let arr=[4,2,5,1,3]
+// let arr=[4,2,5,1,3]
 
 
-arr.sort((a,b)=>b-a)
+// arr.sort((a,b)=>b-a)
 
 // +ve swap
 // -ve no swap
 // 0 no swap
 
-console.log(arr)
+// console.log(arr)
 
 
-const skills=["html","css","js"];
-skills.sort()
-console.log(skills)
+// const skills=["html","css","js"];
+// skills.sort()
+// console.log(skills)
+
+
+/**
+let arr=[1,2,3,4,5];
+
+let mid=Math.floor(arr.length/2)
+
+const newArr=arr.map((element,index)=> {
+
+    if(index < mid)
+    {
+        return element**2;
+    }
+    
+})
+
+console.log(newArr)
+
+ */
+
+
+/**
+let arr=[1,2,3,4,5]
+let mid=Math.floor(arr.length/2)
+
+let x=  arr.filter((element,index,array)=>index < mid)
+
+let y=  arr.filter((element,index,array)=> element !== 3)
+
+console.log(x)
+console.log(y)
+ */
+
 
