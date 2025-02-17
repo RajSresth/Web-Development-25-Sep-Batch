4a259c93bea06dd90ae1d4c1dea88ee8fb0e68ac
Shresth Rajput <rajshresth2@gmail.com>
2025-02-17 13:37:36 +0530

    DOM Part-1

---
+++ Javascript/Day-25 DOM/script.js
@@ -0,0 +1,11 @@
+const body = document.body;
+
+const chombu = document.getElementById("chombu")
+
+
+
+const items = document.getElementsByClassName("item");
+console.log(items[0])
+console.log(items[1])
+console.log(items[2])
+console.log(items[3])
