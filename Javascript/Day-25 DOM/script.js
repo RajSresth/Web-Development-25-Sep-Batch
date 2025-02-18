19a727edb7d2c2ed5e22ca943a2d90a260c611a6
Shresth Rajput <rajshresth2@gmail.com>
2025-02-18 13:45:56 +0530

    DOM Part-2

--- Javascript/Day-25 DOM/script.js
+++ Javascript/Day-25 DOM/script.js
@@ -1,11 +1,61 @@
 const body = document.body;
-
 const chombu = document.getElementById("chombu")
 
 
 
+/**
+ * ! document.getElementsByClassName("")
+ 
 const items = document.getElementsByClassName("item");
-console.log(items[0])
-console.log(items[1])
-console.log(items[2])
-console.log(items[3])
+
+// TODO: Convert Html collection into Array
+// ! Array.from()
+const arr= Array.from(items)
+arr[2].textContent="JavaScript";
+// arr[2].style.color="red";
+// arr[2].style.fontSize="25px";
+arr[2].style.cssText="color:red; font-size:25px;"
+
+
+arr[3].textContent="ReactJs";
+// arr[3].style.color="blue";
+arr[3].style.cssText="color:blue; font-size:30px;"
+*/
+
+
+/**
+ * ! document.getElementsByTagName("")
+
+const div= document.getElementsByTagName("div");
+div[0].style.background="black";
+div[0].style.color="white";
+ */
+
+/**
+ * ! document.querySelector("")
+ 
+
+const div = document.querySelector(".parent")
+
+console.log("Child Nodes",div.childNodes)
+// console.log("Actual Childs",div.children)
+// console.log("No of Childrens",div.childElementCount)
+
+// const lastItem = document.querySelector(".parent :last-child");
+// lastItem.style.textTransform="lowercase";
+
+*/
+
+/**
+ * ! document.querySelectorAll()
+ */
+
+const items1= document.getElementsByClassName("item")  //Html collection
+const items2 = document.querySelectorAll(".item") //Nodelist
+
+
+items2[3].remove();
+
+console.log(items1)
+console.log(items2)
+
