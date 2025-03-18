3f57a92ef0dcbea5f19bdeccdcb714488d34ca44
Shresth Rajput <rajshresth2@gmail.com>
2025-03-18 13:20:55 +0530

    Lec-1 of Node

---
+++ Node JS/Day-1 Common JS/app.js
@@ -0,0 +1,12 @@
+console.log("App js file")
+
+const greet=()=>{
+    console.log("Good Morning Dev..")
+}
+
+const meme="Smjh rhe ho";
+
+module.exports={
+    greet:greet,
+    meme:meme
+}
---
+++ Node JS/Day-1 Common JS/main.js
@@ -0,0 +1,7 @@
+console.log("Inside Main Js");
+const obj=require("./app.js")
+
+
+console.log(obj);
+
+console.log("End of Main Js")
